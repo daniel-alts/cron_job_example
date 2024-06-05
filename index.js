725a8e0505da44960a776265ed6290d0e54879e9
Adesoji Daniel <sojidaniels@gmail.com>
2024-06-05 22:06:07 +0100

    random app

--- index.js
+++ index.js
@@ -1,44 +1,14 @@
-const schedule = require('node-schedule');
+const express = require('express');
 
-const subscriptions = [
-    {
-        user_id: "12345",
-        next_payment_date: '2022-01-14 18:45',
-        credit_card_info: '**** 1234',
-        authorization_code: "43895743",
-        amount: 100,
-        retry_amount: 0
-    },
-    {
-        user_id: "123",
-        next_payment_date: '2022-01-15 18:45'
-    },
-    {
-        user_id: "12",
-        next_payment_date: '2022-01-16 18:45'
-    }
-]
+const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-const job = schedule.scheduleJob('* * * * *', function(){
-  console.log('This is running!');
+const port = process.env.PORT || 8000;
 
-  const subs = getAllSubscriptionsToday()
-
-  for (const sub of subs) {
-    const response = applySub(sub);
-
-    if (response.isSuccessful) {
-        sub.next_payment_date = thirtyDaysFromNow()
-    } else {
-        sub.next_payment_date = tomorrow()
-        sub.retry_amount += 1
-
-        if (sub.retry_amount > 5) {
-            removeFromPremiumAccount(sub.user_id);
-            sub.next_payment_date = null;
-        }
-    }
-  }
-});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
 
